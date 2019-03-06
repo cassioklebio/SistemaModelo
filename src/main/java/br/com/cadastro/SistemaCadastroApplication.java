60fbfd033378adf32af6dfe491e757c37e8e4e92
Cássio Klebio <cassiotr2009@gmail.com>
2019-03-05 22:22:22 -0300

    Produtos e associacao muitos para muitos

--- src/main/java/br/com/cadastro/SistemaCadastroApplication.java
+++ src/main/java/br/com/cadastro/SistemaCadastroApplication.java
@@ -8,13 +8,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import br.com.cadastro.domain.Categoria;
+import br.com.cadastro.domain.Produto;
 import br.com.cadastro.repositories.CategoriaRepository;
+import br.com.cadastro.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class SistemaCadastroApplication implements CommandLineRunner {
 	
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+	
+	@Autowired
+	private ProdutoRepository produtoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SistemaCadastroApplication.class, args);
@@ -27,8 +32,22 @@ public class SistemaCadastroApplication implements CommandLineRunner {
 		Categoria cat1 = new Categoria(null, "Informatica");
 		Categoria cat2 = new Categoria(null, "Escritório");
 		
+		Produto p1 = new Produto(null, "computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00); 
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
+		
 		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
+		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
 	}
 
 }
