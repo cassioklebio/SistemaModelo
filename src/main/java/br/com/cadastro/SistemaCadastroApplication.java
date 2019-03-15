cfe55bd022648c88b067c593fc506f319e462f8e
Cássio Klebio <cassiotr2009@gmail.com>
2019-03-15 15:51:38 -0300

    Cliente, TipoCliente, telefones e enderecos

--- src/main/java/br/com/cadastro/SistemaCadastroApplication.java
+++ src/main/java/br/com/cadastro/SistemaCadastroApplication.java
@@ -9,10 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import br.com.cadastro.domain.Categoria;
 import br.com.cadastro.domain.Cidade;
+import br.com.cadastro.domain.Cliente;
+import br.com.cadastro.domain.Endereco;
 import br.com.cadastro.domain.Estado;
 import br.com.cadastro.domain.Produto;
+import br.com.cadastro.domain.enums.TipoCliente;
 import br.com.cadastro.repositories.CategoriaRepository;
 import br.com.cadastro.repositories.CidadeRepository;
+import br.com.cadastro.repositories.ClienteRepository;
+import br.com.cadastro.repositories.EnderecoRepository;
 import br.com.cadastro.repositories.EstadoRepository;
 import br.com.cadastro.repositories.ProdutoRepository;
 
@@ -30,6 +35,12 @@ public class SistemaCadastroApplication implements CommandLineRunner {
 	
 	@Autowired
 	private CidadeRepository cidadeRepository;
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
+	@Autowired
+	private EnderecoRepository enderecoRepository;
 	
 	
 	
@@ -74,6 +85,20 @@ public class SistemaCadastroApplication implements CommandLineRunner {
 		
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1,c2,c3));
+	
+		
+		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com","36378912377", TipoCliente.PESSOAFISICA);
+		
+		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
+		
+		Endereco e1 = new Endereco(null, "Rua Flores","300","Apto 303","Jardim","38220834", cli1, c1);
+		Endereco e2 = new Endereco(null, "Avenida Matos","105","Sala 800","Centro","38777012", cli1, c1);
+		
+		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+		
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		enderecoRepository.saveAll(Arrays.asList(e1, e2));
+		
 		
 	}
 
