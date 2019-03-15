c771fce99b079af37194b5a1dadd9ddabb6aa163
Cássio Klebio <cassiotr2009@gmail.com>
2019-03-15 11:21:03 -0300

    Estado e Cidade

--- src/main/java/br/com/cadastro/SistemaCadastroApplication.java
+++ src/main/java/br/com/cadastro/SistemaCadastroApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import br.com.cadastro.domain.Categoria;
+import br.com.cadastro.domain.Cidade;
+import br.com.cadastro.domain.Estado;
 import br.com.cadastro.domain.Produto;
 import br.com.cadastro.repositories.CategoriaRepository;
+import br.com.cadastro.repositories.CidadeRepository;
+import br.com.cadastro.repositories.EstadoRepository;
 import br.com.cadastro.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -20,6 +24,15 @@ public class SistemaCadastroApplication implements CommandLineRunner {
 	
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	
+	@Autowired
+	private EstadoRepository estadoRepository;
+	
+	@Autowired
+	private CidadeRepository cidadeRepository;
+	
+	
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(SistemaCadastroApplication.class, args);
@@ -43,11 +56,25 @@ public class SistemaCadastroApplication implements CommandLineRunner {
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 		
-		
-		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		
 		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
+		
+		
+		Estado est1 = new Estado(null,"Minas Gerais");
+		Estado est2 = new Estado(null,"São Paulo");
+		
+		Cidade c1 = new Cidade(null, "Uberlândia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+		
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2, c3));
+		
+		
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1,c2,c3));
+		
 	}
 
 }
