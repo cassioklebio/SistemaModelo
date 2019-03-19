30bed930df22f4b45801b38ce84a16f7cd08ed51
Cássio Klebio <cassiotr2009@gmail.com>
2019-03-19 15:31:58 -0300

    ItemPedido e ItemPedidoPK

--- src/main/java/br/com/cadastro/SistemaCadastroApplication.java
+++ src/main/java/br/com/cadastro/SistemaCadastroApplication.java
@@ -13,6 +13,7 @@ import br.com.cadastro.domain.Cidade;
 import br.com.cadastro.domain.Cliente;
 import br.com.cadastro.domain.Endereco;
 import br.com.cadastro.domain.Estado;
+import br.com.cadastro.domain.ItemPedido;
 import br.com.cadastro.domain.Pagamento;
 import br.com.cadastro.domain.PagamentoComBoleto;
 import br.com.cadastro.domain.PagamentoComCartao;
@@ -25,6 +26,7 @@ import br.com.cadastro.repositories.CidadeRepository;
 import br.com.cadastro.repositories.ClienteRepository;
 import br.com.cadastro.repositories.EnderecoRepository;
 import br.com.cadastro.repositories.EstadoRepository;
+import br.com.cadastro.repositories.ItemPedidoRepository;
 import br.com.cadastro.repositories.PagamentoRepository;
 import br.com.cadastro.repositories.PedidoRepository;
 import br.com.cadastro.repositories.ProdutoRepository;
@@ -56,6 +58,9 @@ public class SistemaCadastroApplication implements CommandLineRunner {
 	
 	@Autowired
 	private PagamentoRepository pagamentoRepository;
+	
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
 	
 	
 
@@ -130,6 +135,23 @@ public class SistemaCadastroApplication implements CommandLineRunner {
 		
 		pedidoRepository.saveAll(Arrays.asList(ped1,ped2));
 		pagamentoRepository.saveAll(Arrays.asList(pagto1,pagto2));
+		
+		
+		ItemPedido ip1 = new ItemPedido(ped1, p1, 0.00, 1, 2000.00);
+		ItemPedido ip2 = new ItemPedido(ped1, p3, 0.00, 2, 80.00);
+		ItemPedido ip3 = new ItemPedido(ped2, p2, 100.00, 1, 800.00);
+		
+		
+		ped1.getItens().addAll(Arrays.asList(ip1, ip2));
+		ped2.getItens().addAll(Arrays.asList(ip3));
+		
+		p1.getItens().addAll(Arrays.asList(ip1));
+		p2.getItens().addAll(Arrays.asList(ip3));
+		p3.getItens().addAll(Arrays.asList(ip1));
+		
+		itemPedidoRepository.saveAll(Arrays.asList(ip1, ip2, ip3));
+		
+		
 		
 		
 	}
