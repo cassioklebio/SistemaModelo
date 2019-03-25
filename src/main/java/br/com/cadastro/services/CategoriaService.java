daeba8ec86038945269ff9baa1d4c6d59d1341a1
Cássio Klebio <cassiotr2009@gmail.com>
2019-03-25 13:35:19 -0300

    Listando todas categorias com DTO

--- src/main/java/br/com/cadastro/services/CategoriaService.java
+++ src/main/java/br/com/cadastro/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package br.com.cadastro.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -44,6 +45,10 @@ public class CategoriaService {
 			throw new DataIntegrityException("Não é possivel excluir uma categoria que possui produtos vinculados");
 		}
 		
+	}
+	
+	public List<Categoria> findAll(){
+		return repo.findAll();
 	}
 	
 	
